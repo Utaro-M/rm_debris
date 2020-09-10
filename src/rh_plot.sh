75dc7581952eda867fd48a2d0d0695306d822723
utaro <utaro.matsuura@gmail.com>
2020-09-10 14:17:02 +0900

    plot用のyaml

---
+++ src/rh_plot.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# これはコメントです
+
+echo $1
+cd /home/utaro/matsuura@jsk.imi.i.u-tokyo.ac.jp/Documents/log/JAXON_RED
+cd $1
+# python ~/catkin_ws/jaxon_tutorial/src/log_plotter/src/log_plotter/datalogger_plotter_with_pyqtgraph.py -f contact_60_7_JAXON_RED_2020_09-08_22-03_44 --plot ~/research/rm_debris/log/rh_plot.yaml --layout ~/research/rm_debris/log/rh_layout.yaml
+
+python ~/catkin_ws/jaxon_tutorial/src/log_plotter/src/log_plotter/datalogger_plotter_with_pyqtgraph.py -f $1 --plot ~/research/rm_debris/log/rh_plot.yaml --layout ~/research/rm_debris/log/rh_layout.yaml
